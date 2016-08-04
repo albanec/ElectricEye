3632f69029b2c38602b8a3b914ba181ad45db3cb
albanec <eugeni.maksimov@yandex.ru>
2016-08-04 15:33:00 +0300

    добавлен автоматический парсинг нужных столбцов в файлах

--- main_script.R
+++ main_script.R
@@ -1,20 +1,24 @@
 #
-## подгрузка пакетов
+### подгрузка пакетов
 library(quantmod)
 library(rusquant)
 library(magrittr)
 library(plotly)
 #
-## подгрузка библиотек
+### подгрузка библиотек
 source("lib/libGeneric.R")
 source("lib/libCluster.R")
 source("lib/libLabsAnalysis.R")
 #
-## исходные данные
+### исходные данные
 # путь к обрабатываемому файлу 
 file.path <- "temp/test_data.csv"
-# номера столбцов с переменными 
+# номера столбцов с переменными и профитом/просадкой
 var.list <- c(26, 27, 28)
+profit <- 3
+draw <- 9
+# автоматический парсинг номеров нужных номеров столбцов
+autoParse <- TRUE 
 # число месяцев торговли (зависит от оптимизации в tslab)
 m <- 12
 # квантиль доходности (по умолчанию выставлено наиболее оптимальное значение)
@@ -25,32 +29,44 @@ varDigits <- 2
   # хоть и менее точен))
 kmeanpp <- FALSE
 #
-## обработка .csv файла 
+#### обработка .csv файла 
 # (полный набор обработки - парсинг, исправление возможных ошибок 
   # и добавление нормированного к просадке и году доходу, вычисление квантиля по доходу)
 data <- AllPreparation_LabsFile(file.path = file.path, 
-                                var.list = var.list, profit = 3, draw = 9, 
+                                autoParse, 
                                 m = m, 
                                 q.hi = qLevel, hi = TRUE,
                                 one.scale = TRUE)
 data$profit <- NULL
 data$draw <- NULL
 #
-## вычисление кластеров
+### вычисление кластеров
 # вычисление параметров кластеризации 
 clustPar.data <- CalcKmean_Parameters(data, iter.max = 100, plusplus = kmeanpp, test.range = 30)
 # вычисление самох кластеров
 clustFull.data <- CalcKmean(data, clustPar.data[[2]], plusplus = kmeanpp, var.digits = varDigits)
 # вывод данных
-clustFull.data[2]
+print(clustFull.data[2])
 #
-## графика
+### графика
 # визуализация вычисления оптимального числа кластеров
 PlotKmean_SS(ss.df = clustPar.data[1], n.opt = clustPar.data[[2]])
 # визуализация кластеров
-PlotKmean_Clusters(data.list=clustFull.data, cluster.color = FALSE, dimension = "3d", 
+  # если cluster.color = FALSE  - точки красятся по профиту
+  # если cluster.color = TRUE - точки красятся по кластеру
+dim <- ifelse(n.vars == 3,
+              "3d",
+              ifelse(length(var.list) == 2,
+                     "2d",
+                     NA))
+if (is.na(dim)) {
+  stop(paste("PlotKmean_Clusters:  Dimension error!!!", sep = ""))
+} else {
+  PlotKmean_Clusters(data.list=clustFull.data, cluster.color = FALSE, dimension = dim, 
                    plot.title = "ClustersPlot", xaxis.name = "FastMA", yaxis.name = "SlowMA", 
                    zaxis.name = "PER", 
                    point.size = 4, point.opacity = 1, point.line.width = 0.7, point.line.opacity = 0.5,
-                   center.size = 20, center.color = "black") 
+                   center.size = 20, center.color = "black")
+}
+ 
 #
